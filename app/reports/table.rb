8ebe0332db1a305bd73a283c97073ef0a7460519
selena <selena@analyticsmediagroup.com>
2014-06-12 13:45:02 -0400

    displaying top 3 sort keys and dist key

--- app/reports/table.rb
+++ app/reports/table.rb
@@ -11,14 +11,17 @@ module Reports
   
     def result
       sql = <<-SQL
-      SELECT c.schemaname as schema, c.tablename as table, c.tableid, c.size_in_mb, a.attname as sortkey, b.attname as distkey,
-      c.has_col_encoding, c.pct_skew_across_slices, c.pct_slices_populated FROM statistics.tables_report c 
-      left join 
-      (select attname,attrelid from pg_attribute where attsortkeyord is true) as a
-      on c.tableid = a.attrelid
-      left join
-      (select attname,attrelid from pg_attribute where attisdistkey is true) as b
-      on c.tableid = b.attrelid
+      SELECT c.schemaname as schema, c.tablename as table, c.tableid, c.size_in_mb, 
+      sort_key_1_attr.attname as sort_key_1,
+      sort_key_2_attr.attname as sort_key_2,
+      sort_key_3_attr.attname as sort_key_3,
+      dist_key_attr.attname as dist_key,
+      c.has_col_encoding, c.pct_skew_across_slices, c.pct_slices_populated
+      FROM statistics.tables_report c 
+      left join pg_attribute as sort_key_1_attr on sort_key_1_attr.attrelid = c.tableid and sort_key_1_attr.attsortkeyord = 1
+      left join pg_attribute as sort_key_2_attr on sort_key_2_attr.attrelid = c.tableid and sort_key_2_attr.attsortkeyord = 2
+      left join pg_attribute as sort_key_3_attr on sort_key_3_attr.attrelid = c.tableid and sort_key_3_attr.attsortkeyord = 3
+      left join pg_attribute as dist_key_attr on dist_key_attr.attrelid = c.tableid and dist_key_attr.attisdistkey is true
       order by c.size_in_mb desc;
       SQL
       @result = self.class.connection.select_all(self.class.sanitize([sql, @options]))
