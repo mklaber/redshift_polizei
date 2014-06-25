9842d17cf5a652d81e9db514c6d4065cec9abe39
Alberto Benavides <alberto@alberto.local>
2014-06-25 18:08:06 -0400

    Adding pagination for long tables

---
+++ app/assets/javascripts/pagination.js
@@ -0,0 +1,74 @@
+//All the JS here basically handles page navigation for tables
+//that have a bunch of rows
+$(document).ready(function() {
+       
+    //This will bring us to the correct page in the table
+    function goToPage(pageNumber) {
+        $('.table tr:not(:has(th))').hide();
+        var startIndex = (pageNumber-1)*rowLimit;
+        for(var i = startIndex; i < startIndex + rowLimit; i++) {
+            $(tableRows[i]).show();
+        }
+    }
+    
+    //We need to keep track of which page we're on and set a limit to number of rows per page
+    var currentPage = 1;
+    var rowLimit = 10;    
+    
+    //We want to limit the number of rows we see in a table
+    var tableRows = $('.table tr:not(:has(th))');
+    for(var i = 0; i < tableRows.length; i++) {
+        if(i >= rowLimit) {
+            $(tableRows[i]).hide();
+        }
+    }
+    
+    //We also want a pagination at the bottom
+    var numPages = Math.ceil(tableRows.length /rowLimit);
+    var pageTabs = "<div class='pageTabber'>";
+    pageTabs = pageTabs + "<a class='pageNav' id='fullBack' href='javascript:void(0);'> << </a>";
+    pageTabs = pageTabs + "<a class='pageNav' id='oneBack' href='javascript:void(0);'> < </a>";
+    pageTabs = pageTabs + "Page &nbsp;" + "<span id='pageNumber'>" + currentPage + "</span> &nbsp; / &nbsp;" + numPages;
+    pageTabs = pageTabs + "<a class='pageNav' id='oneForward' href='javascript:void(0);'> > </a>";
+    pageTabs = pageTabs + "<a class='pageNav' id='fullForward' href='javascript:void(0);'> >> </a>";
+    pageTabs = pageTabs + "</div>";
+    $('.table').after(pageTabs);
+    
+    //We now add the onclick listeners so each pagination arrow does it's thing
+    $(".pageTabber a").click(function() {
+        var id = $(this).attr("id");
+        if(id == "fullBack") {
+            currentPage = 1;
+        }
+        else if (id == "oneBack") {
+            if(currentPage != 1) {
+                currentPage = currentPage - 1;
+            }
+        }
+        else if (id == "fullForward") {
+            currentPage = numPages;
+        }
+        else {
+            if(currentPage != numPages) {
+                currentPage = currentPage + 1;
+            }
+        }
+        goToPage(currentPage);
+        $(".pageTabber #pageNumber").text(currentPage);    
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
