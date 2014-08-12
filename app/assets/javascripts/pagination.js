680d114e30e12b9df72b08a815111411c4ad1ef4
Alberto Benavides <alberto@alberto.local>
2014-08-12 10:46:43 -0400

    Updating pagination

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,73 +1,56 @@
 //This function will paginate everything with the class "paginateMe"  
 function paginate_stuff() {
     
-    //We need to keep track of which page we're on and set a limit to number of rows per page
-    var currentPage = 1;
-    var rowLimit = 10;    
-    
-    //In the case of a page refresh, we merely want to update the pagination results and nothing else
-    if(arguments.length > 0 && arguments[0] == true) {
-        goToPage(currentPage);
-        return;
+    //Synchronizes the bottom and top navigation bars hackishly
+    function syncPageNavs() {
+        var pageNavs  = $(".pagination").get();
+        $(pageNavs[1]).remove();
+        $(".paginateMe").after($(pageNavs[0]).clone());
+        $(".pageTab").unbind();
+        $(".pageTab").click(pageTabClickListener);
+        $(".arrowNav").unbind();
+        $(".arrowNav").click(arrowClickListener);
     }
     
     //This will bring us to the correct page in the table
-    function goToPage(pageNumber) {
+    function goToPage(currentPage, pageRequested) {
+        
         $('.paginateMe tr:not(:has(th))').hide();
         var rows = $('.paginateMe tr:not(:has(th))');
-        var startIndex = (pageNumber-1)*rowLimit;
+        var startIndex = (pageRequested-1)*rowLimit;
         for(var i = startIndex; i < startIndex + rowLimit; i++) {
             $(rows[i]).show();
         }
-    }
-    
-    //We want to limit the number of rows we see in a table
-    var tableRows = $('.paginateMe tr:not(:has(th))');
-    for(var i = 0; i < tableRows.length; i++) {
-        if(i >= rowLimit) {
-            $(tableRows[i]).hide();
+        $(".pageTab#" + pageRequested).parent().attr("class", "active");
+        $(".pageTab#" + currentPage).parent().attr("class", "");
+        
+        //Check to see if we have to shift the pageTabs left or right
+        //Calculate the shift amount required and do it
+        var rightEdge = Number($(".pageTab:eq(" + (pageLimit-1) + ")").attr("id"));
+        var leftEdge = Number($(".pageTab:eq(0)").attr("id"));
+        if(rightEdge <= pageRequested && isLotsPages) {
+            var shift = Math.min(Math.ceil(pageLimit/2), numPages - pageRequested);
+            if(pageRequested > rightEdge) shift = pageRequested - rightEdge;
+            updateLongPageNav(pageRequested, shift);
+        } 
+        else if(leftEdge >= pageRequested && isLotsPages && leftEdge > 1) {
+            var shift = Math.max(-Math.ceil(pageLimit/2), 1 - pageRequested);
+            if(pageRequested < leftEdge) shift = pageRequested - leftEdge;  
+            updateLongPageNav(pageRequested, shift);
         }
     }
     
-    //We also want a pagination at the bottom
-    var numPages = Math.ceil(tableRows.length /rowLimit);
-    var pageTabs = "<ul class='pagination'>";
-    pageTabs = pageTabs + "<li class='disabled'><a id='fullBack' href='javascript:void(0);'> << </a></li>";
-    pageTabs = pageTabs + "<li class='disabled'><a id='oneBack' href='javascript:void(0);'> < </a></li>";
-    pageTabs = pageTabs + "<li class='disabled'>";
-    pageTabs = pageTabs + "<a id='pageInfo' href='javascript:void(0);'>Page &nbsp;"+currentPage+" &nbsp; / &nbsp;"+numPages+"</a></li>";
-    pageTabs = pageTabs + "<li><a id='oneFront' href='javascript:void(0);'> > </a></li>";
-    pageTabs = pageTabs + "<li><a id='fullFront' href='javascript:void(0);'> >> </a></li>";
-    pageTabs = pageTabs + "</ul>";
-    $('.paginateMe').after(pageTabs);
-    
-    //We now add the onclick listeners so each pagination arrow does it's thing
-    $(".pagination a").click(function() {
-        //First, we make sure that this page navigation event isn't disabled
-        var isDisabled = $(this).parent().attr("class") == "disabled";
-        if(isDisabled) return;
-        //Depending on the pagination arrow clicked, we update the page counter
-        var id = $(this).attr("id");
-        if(id == "fullBack") {
-            currentPage = 1;
-        }
-        else if(id == "oneBack") {
-            currentPage = currentPage - 1;
-        }
-        else if(id == "oneFront") {
-            currentPage = currentPage + 1;
-        }
-        else {
-            currentPage = numPages;
-        }
+    //Disable navigation arrows if current page makes it reasonable to do so
+    //As an example, if you are pn page 1, the left arrows should not be active
+    function updateArrows(pageNumber) {
         //We disable the page navigators if we reach either edge of the results
-        if(currentPage == 1) {
+        if(pageNumber == 1) {
             $("li:has(a#oneBack)").attr("class", "disabled");
             $("li:has(a#fullBack)").attr("class", "disabled");
             $("li:has(a#oneFront)").attr("class", "");
             $("li:has(a#fullFront)").attr("class", "");
         }
-        else if(currentPage == numPages) {
+        else if(pageNumber == numPages) {
             $("li:has(a#oneBack)").attr("class", "");
             $("li:has(a#fullBack)").attr("class", "");
             $("li:has(a#oneFront)").attr("class", "disabled");
@@ -79,10 +62,132 @@ function paginate_stuff() {
             $("li:has(a#oneFront)").attr("class", "");
             $("li:has(a#fullFront)").attr("class", "");
         }
-        //We update the page results
-        goToPage(currentPage);
-        $("#pageInfo").text("Page " + currentPage + " / " + numPages);    
-    });
+    }
+    
+    //Create the navigation bar
+    function createPageNavigation(numPages) {
+        var pageTabs = "<ul class='pagination'>";
+        pageTabs = pageTabs + "<li class='disabled'><a class='arrowNav' id='fullBack' href='javascript:void(0);'> << </a></li>";
+        pageTabs = pageTabs + "<li class='disabled'><a class='arrowNav' id='oneBack' href='javascript:void(0);'> < </a></li>";
+        
+        if(numPages > pageLimit) {
+            isLotsPages = true;
+            for(var i = 1; i <= pageLimit; i++) {
+                if(i == 1) {
+                    pageTabs = pageTabs + "<li class='active'>";
+                } else {
+                    pageTabs = pageTabs + "<li>";
+                }
+                pageTabs = pageTabs + "<a class='pageTab' id='" + i + "' href='javascript:void(0);'>" + i + "</a>";
+                pageTabs = pageTabs + "</li>";
+            }            
+        } 
+        else {
+            for(var i = 1; i <= numPages; i++) {
+                if(i == 1) {
+                    pageTabs = pageTabs + "<li class='active'>";
+                } else {
+                    pageTabs = pageTabs + "<li>";
+                }
+                pageTabs = pageTabs + "<a class='pageTab' id='" + i + "' href='javascript:void(0);'>" + i + "</a>";
+                pageTabs = pageTabs + "</li>";
+            }
+        }
+        
+        pageTabs = pageTabs + "<li><a class='arrowNav' id='oneFront' href='javascript:void(0);'> > </a></li>";
+        pageTabs = pageTabs + "<li><a class='arrowNav' id='fullFront' href='javascript:void(0);'> >> </a></li>";
+        pageTabs = pageTabs + "</ul>";
+        return pageTabs;
+    }
+
+    //If there's a lot of pages, we want to using a sliding door to view the individual pages
+    function updateLongPageNav(pageNumber, shift) {
+        
+        //Now, we add the pageTabs to the left
+        for(var i = 0; i < pageLimit; i++) {
+            var pageTab = $(".pagination a.pageTab:eq(" + i + ")");
+            var newID = Number($(pageTab).attr("id")) + shift;
+            $(pageTab).attr("id", newID);
+            $(pageTab).text(newID);
+            if(newID == pageNumber) {
+                $(pageTab).parent().attr("class", "active");
+            } else {
+                $(pageTab).parent().attr("class", "");
+            }
+        }
+    }
+    
+    //OnClickListener for each page tab
+    function pageTabClickListener() {
+        var isDisabled = $(this).parent().attr("class") == "active";
+        if(isDisabled) return;
+        
+        pageRequested = Number($(this).attr("id"));
+        goToPage(currentPage, pageRequested);
+        updateArrows(pageRequested);
+        currentPage = pageRequested;
+        syncPageNavs();
+    }
+
+    //OnClickListener for each arrow in the page navigation bar
+    function arrowClickListener() {
+        
+        //First, we make sure that this page navigation event isn't disabled
+        var isDisabled = $(this).parent().attr("class") == "disabled";
+        if(isDisabled) return;
+        
+        //Depending on the pagination arrow clicked, we update the page counter
+        var id = $(this).attr("id");
+        var pageRequested = 1;
+        if(id == "fullBack") {
+            pageRequested = 1;
+        }
+        else if(id == "oneBack") {
+            pageRequested = currentPage - 1;
+        }
+        else if(id == "oneFront") {
+            pageRequested = currentPage + 1;
+        }
+        else {
+            pageRequested = numPages;
+        }
+        goToPage(currentPage, pageRequested);
+        updateArrows(pageRequested);
+        currentPage = pageRequested;
+        syncPageNavs();
+    }
+    
+    //We need to keep track of which page we're on and set a limit to number of rows per page
+    var currentPage = 1;
+    var rowLimit = 20;    
+    var pageLimit = 10;    
+    var isLotsPages = false;    
+    
+    //In the case of a page refresh, we merely want to update the pagination results and nothing else
+    if(arguments.length > 0 && arguments[0] == true) {
+        goToPage(2, 1);
+        return;
+    }
+    
+    //We want to limit the number of rows we see in a table
+    var tableRows = $('.paginateMe tr:not(:has(th))');
+    for(var i = 0; i < tableRows.length; i++) {
+        if(i >= rowLimit) {
+            $(tableRows[i]).hide();
+        }
+    }
+    
+    //Add a navigation bar at the bottom
+    //Display a tab for each page if number of pages < page limit
+    //Otherwise, show 3 tabs before and after current page
+    var numPages = Math.ceil(tableRows.length /rowLimit);
+    var pageTabs = createPageNavigation(numPages);
+    $('.paginateMe').after(pageTabs);
+    $(".paginateMe").before(pageTabs);
+    
+    //Add onclick listeners
+    $(".pageTab").click(pageTabClickListener);
+    $(".arrowNav").click(arrowClickListener);
 } 
 
 
