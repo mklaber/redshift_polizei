6183c11057df41c5145a92b86984c702ddce00ad
Tobias Thiel <tobias011@gmail.com>
2014-12-10 21:46:41 +0100

    logger update for deployment

--- app/logger.rb
+++ app/logger.rb
@@ -1,15 +1,42 @@
 require 'logger'
 
+class MultiIO
+  def initialize(*targets)
+     @targets = targets
+  end
+
+  def write(*args)
+    @targets.each {|t| t.write(*args)}
+  end
+
+  def flush
+    @targets.each do |t|
+      t.flush if t.respond_to? :flush
+    end
+  end
+
+  def close
+    @targets.each(&:close)
+  end
+end
+
 #
 # common Logger for Sinatra, ActiveRecord and everything else
 #
 class PolizeiLogger < Logger
+  def initialize(logdev)
+    super
+    @logdev = logdev
+  end
+
   #
-  # singelton method
+  # singleton method
   #
   def self.logger
     if @_logger.nil?
-      @_logger = self.new STDOUT
+      logfile = "../log/#{Sinatra::Application.environment}.log"
+      f = File.open(File.expand_path(logfile, File.dirname(__FILE__ )), "a")
+      @_logger = self.new MultiIO.new(STDOUT, f)
       @_logger.level = Logger::DEBUG
       @_logger.datetime_format = '%FT%T.%N%z'
     end
@@ -21,7 +48,7 @@ class PolizeiLogger < Logger
   alias :puts :'<<'
 
   def flush
-    # TODO needed on deployment machine
+    @logdev.flush
   end
 
   #
