fbbfba41e4d355661f95936488f94b8f9e0e7864
Tobias Thiel <tobias011@gmail.com>
2014-12-10 17:15:45 +0100

    minor javascript fix, waiting on document ready

--- app/assets/javascripts/queries.js
+++ app/assets/javascripts/queries.js
@@ -1,48 +1,50 @@
-datatable_init($('#queries_running_tbl')[0], {
-  'columnDefs': [
-    {
-      'targets': 0,
-      'render': function (data, type, row) {
-        return row['username'] + '<small class="secondary"> (' + row['user_id'] + ')</small>';
-      }
-    },
-    {
-      'targets': 1,
-      'data': 'status'
-    },
-    {
-      'targets': 2,
-      'render': function(data, type, row) {
-        var date = $.format.date(row['start_time'], 'dd.MM.yy');
-        var time = $.format.date(row['start_time'], 'HH:mm:ss');
-        return date + ' at ' + time + ' UTC';
-      }
-    },
-    {
-      'targets': 3,
-      'render': function(data, type, row) {
-        var duration;
-        if (row['duration']) {
-          duration = row['duration'];
-        } else {
-          duration = (new Date().getTime() / 1000) - row['start_time'];
+$(document).ready(function() {
+  datatable_init($('#queries_running_tbl')[0], {
+    'columnDefs': [
+      {
+        'targets': 0,
+        'render': function (data, type, row) {
+          return row['username'] + '<small class="secondary"> (' + row['user_id'] + ')</small>';
         }
-        if (duration < 0) duration = 0;
-        var mins = Math.round(duration / 60);
-        var secs = Math.round(duration % 60);
-        if (mins > 0)
-          return mins + ' min ' + secs + ' sec';
-        else
-          return secs + ' sec';
+      },
+      {
+        'targets': 1,
+        'data': 'status'
+      },
+      {
+        'targets': 2,
+        'render': function(data, type, row) {
+          var date = $.format.date(row['start_time'], 'dd.MM.yy');
+          var time = $.format.date(row['start_time'], 'HH:mm:ss');
+          return date + ' at ' + time + ' UTC';
+        }
+      },
+      {
+        'targets': 3,
+        'render': function(data, type, row) {
+          var duration;
+          if (row['duration']) {
+            duration = row['duration'];
+          } else {
+            duration = (new Date().getTime() / 1000) - row['start_time'];
+          }
+          if (duration < 0) duration = 0;
+          var mins = Math.round(duration / 60);
+          var secs = Math.round(duration % 60);
+          if (mins > 0)
+            return mins + ' min ' + secs + ' sec';
+          else
+            return secs + ' sec';
+        }
+      },
+      {
+        'targets': 4,
+        'data': 'xid'
+      },
+      {
+        'targets': 5,
+        'data': 'query'
       }
-    },
-    {
-      'targets': 4,
-      'data': 'xid'
-    },
-    {
-      'targets': 5,
-      'data': 'query'
-    }
-  ]
+    ]
+  });
 });
