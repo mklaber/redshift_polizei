4499c412c76d3532e99b63b70f5ed587b0a35a36
Tobias Thiel <tobias011@gmail.com>
2014-12-11 17:34:16 +0100

    proper logging with rotation outside of development

--- app/logger.rb
+++ app/logger.rb
@@ -2,50 +2,24 @@ require 'logger'
 require 'fileutils'
 
 #
-# Passes IO actions to multiple IO backends
+# removes color escape character sequences from log messages
+# and write them to file
 #
-class MultiIO
-  def initialize(*targets)
-     @targets = targets
+class ColorBlindFile
+  def initialize(*args)
+     @target = File.new(*args)
   end
 
   def write(*args)
-    @targets.each {|t| t.write(*args)}
+    @target.write(*args.map {|x| x.gsub(/\e\[(\d+)m/, '')}.compact)
   end
 
   def flush
-    @targets.each do |t|
-      # not every backend will support flush
-      t.flush if t.respond_to? :flush
-    end
+    @target.flush if @target.respond_to? :flush
   end
 
   def close
-    @targets.each(&:close)
-  end
-end
-
-#
-# removes color escape character sequence from string
-#
-class ColorBlind
-  def initialize(*targets)
-     @targets = targets
-  end
-
-  def write(*args)
-    @targets.each {|t| t.write(*args.map {|x| x.gsub(/\e\[(\d+)m/, '')}.compact)}
-  end
-
-  def flush
-    @targets.each do |t|
-      # not every backend will support flush
-      t.flush if t.respond_to? :flush
-    end
-  end
-
-  def close
-    @targets.each(&:close)
+    @target.close
   end
 end
 
@@ -63,29 +37,32 @@ class PolizeiLogger < Logger
   #
   def self.logger
     if @_logger.nil?
-      # create log directory if it doesn't exist
-      logdirectory = '../log'
-      FileUtils::mkdir_p File.expand_path(logdirectory, File.dirname(__FILE__ ))
-      # generate log file name
+      # create logger
       env = Sinatra::Application.environment
-      logfile = "#{logdirectory}/#{env}.log"
-      # open environment log file while removing color coding from messages
-      f = File.open(File.expand_path(logfile, File.dirname(__FILE__ )), "a")
-      # synchronous log file IO for development
-      f.sync = true if env == :development
-      fc = ColorBlind.new(f)
-      # set up logger
-      @_logger = self.new MultiIO.new(STDOUT, fc)
+      if env == :development
+        # development logs to stdout
+        @_logger = self.new STDOUT
+      else
+        # everything else to file
+        # create log directory if it doesn't exist
+        logdirectory = '../log'
+        FileUtils::mkdir_p File.expand_path(logdirectory, File.dirname(__FILE__ ))
+        # generate log file name
+        logfile = "#{logdirectory}/#{env}.log"
+        # open environment log file while removing color coding from messages
+        f = File.open(File.expand_path(logfile, File.dirname(__FILE__ )), "a")
+        @_logger = self.new(ColorBlind.new(f), shift_age='daily')
+      end
+      # set up logger settings
       @_logger.level = Logger::DEBUG
       @_logger.datetime_format = '%FT%T.%N%z'
     end
     @_logger
   end
 
-  # Rack expects a write method for its logger
+  # Rack expects a write, puts and flush method for its logger
   alias :write :'<<'
   alias :puts :'<<'
-
   def flush
     @logdev.flush
   end
