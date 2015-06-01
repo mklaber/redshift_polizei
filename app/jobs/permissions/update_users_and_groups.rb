d3db2d66a4b257604d3281ae81053ff45b9992b2
Tobias Thiel <tobias011@gmail.com>
2015-06-01 15:26:46 +0200

    permissions update should support user/group renames

--- app/jobs/permissions/update_users_and_groups.rb
+++ app/jobs/permissions/update_users_and_groups.rb
@@ -16,11 +16,11 @@ module Jobs
         Models::DatabaseGroup.transaction do
           results.each do |data|
             begin
-              group = Models::DatabaseGroup.find_or_initialize_by(name: data['group'])
-              group.update!(database_id: data['group_id'])
+              group = Models::DatabaseGroup.find_or_initialize_by(database_id: data['group_id'])
+              group.update!(name: data['group'])
               group.touch
-              user = Models::DatabaseUser.find_or_initialize_by(name: data['username'])
-              user.update!(database_id: data['user_id'], superuser: (data['is_superuser'] == 't'))
+              user = Models::DatabaseUser.find_or_initialize_by(database_id: data['user_id'])
+              user.update!(name: data['username'], superuser: (data['is_superuser'] == 't'))
               user.touch
               unless Models::DatabaseGroupMemberships.where(user: user, group: group).exists?
                 Models::DatabaseGroupMemberships.create!(user: user, group: group)
