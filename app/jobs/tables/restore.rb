9dbd0bcdd6f1f1a2296ecf74368125a1ac5a6bb0
Tobias Thiel <tobias011@gmail.com>
2015-08-11 12:38:55 +0200

    debug output what restore job is executing

--- app/jobs/tables/restore.rb
+++ app/jobs/tables/restore.rb
@@ -96,6 +96,8 @@ module Jobs
           CREDENTIALS 'aws_access_key_id=#{access_key};aws_secret_access_key=#{secret_key}'
           MANIFEST #{copy_options};
       SQL
+      p 'copy_sql'
+      p copy_sql
       RSPool.with do |conn|
         conn.exec(copy_sql)
       end
