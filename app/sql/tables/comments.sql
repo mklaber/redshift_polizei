332c025724819f8ec52a729d7d505a72ce0c7349
Dan Radion <dan.radion@gmail.com>
2015-09-06 23:34:22 -0400

    fixed column comments displaying as table comments

--- app/sql/tables/comments.sql
+++ app/sql/tables/comments.sql
@@ -6,4 +6,5 @@ from pg_class c
 join pg_description d on c.oid = d.objoid
 join pg_namespace n on n.oid = c.relnamespace
 -- filter out system tables, temp tables, and indexes
-where c.reltype != 0 and n.nspname not in ('pg_catalog', 'information_schema', 'pg_toast') and n.nspname not like 'pg_temp_%%'
+where c.reltype != 0 and d.objsubid and
+  n.nspname not in ('pg_catalog', 'information_schema', 'pg_toast') and n.nspname not like 'pg_temp_%%'
