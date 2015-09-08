cb5e0239d83b3c195e102a9665cfd0f9072e8264
Dan Radion <dan.radion@gmail.com>
2015-09-08 11:59:59 -0400

    fixed comments.sql

--- app/sql/tables/comments.sql
+++ app/sql/tables/comments.sql
@@ -6,5 +6,5 @@ from pg_class c
 join pg_description d on c.oid = d.objoid
 join pg_namespace n on n.oid = c.relnamespace
 -- filter out system tables, temp tables, and indexes
-where c.reltype != 0 and d.objsubid and
+where c.reltype != 0 and d.objsubid = 0 and
   n.nspname not in ('pg_catalog', 'information_schema', 'pg_toast') and n.nspname not like 'pg_temp_%%'
